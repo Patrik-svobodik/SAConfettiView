a63f7d0ebe08a96f83440cb144a8b3c00a884ae2
Patrik Svoboda <svob.pata@gmail.com>
2020-06-22 11:30:38 +0200

    Swift 5 update

--- Pod/Classes/SAConfettiView.swift
+++ Pod/Classes/SAConfettiView.swift
@@ -19,59 +19,36 @@ public class SAConfettiView: UIView {
         case image(UIImage)
     }
 
-    var emitter: CAEmitterLayer!
-    public var colors: [UIColor]!
-    public var intensity: Float!
-    public var type: ConfettiType!
-    private var active :Bool!
+    var emitter = CAEmitterLayer()
+    public var colors = [
+        UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
+        UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
+        UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
+        UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
+        UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)
+    ]
+    public var intensity: Float = 0.5
+    public var type: ConfettiType = .confetti
+    private(set) var active = false
 
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-
-    func setup() {
-        colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
-            UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
-            UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
-            UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
-            UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
-        intensity = 0.5
-        type = .confetti
-        active = false
-    }
 
     public func startConfetti() {
-        emitter = CAEmitterLayer()
-
         emitter.emitterPosition = CGPoint(x: frame.size.width / 2.0, y: 0)
-        emitter.emitterShape = kCAEmitterLayerLine
+        emitter.emitterShape = CAEmitterLayerEmitterShape.line
         emitter.emitterSize = CGSize(width: frame.size.width, height: 1)
 
-        var cells = [CAEmitterCell]()
-        for color in colors {
-            cells.append(confettiWithColor(color: color))
-        }
-
-        emitter.emitterCells = cells
+        emitter.emitterCells = colors.map { confettiWithColor(color: $0) }
         layer.addSublayer(emitter)
         active = true
     }
 
     public func stopConfetti() {
-        emitter?.birthRate = 0
+        emitter.birthRate = 0
         active = false
     }
 
     func imageForType(type: ConfettiType) -> UIImage? {
-
-        var fileName: String!
-
+        var fileName: String
         switch type {
         case .confetti:
             fileName = "confetti"
@@ -90,8 +67,7 @@ public class SAConfettiView: UIView {
         let imagePath = bundle?.path(forResource: fileName, ofType: "png")
         let url = URL(fileURLWithPath: imagePath!)
         do {
-            let data = try Data(contentsOf: url)
-            return UIImage(data: data)
+            return UIImage(data: try Data(contentsOf: url))
         } catch {
             print(error)
         }
@@ -114,9 +90,5 @@ public class SAConfettiView: UIView {
         confetti.scaleSpeed = CGFloat(-0.1 * intensity)
         confetti.contents = imageForType(type: type)!.cgImage
         return confetti
-    }
-
-    public func isActive() -> Bool {
-    		return self.active
     }
 }
